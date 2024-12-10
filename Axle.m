906784c9eb7f3e6702ad5f5745a9a0bb34f53408
Lokecrafter <loke@musteryd.se>
2024-12-10 15:43:33 +0100

    Did more load cases

--- Axle.m
+++ Axle.m
@@ -1,6 +1,6 @@
 classdef Axle
     properties
-        length {mustBeNumeric}
+        length_axle {mustBeNumeric}
         distance_bearing {mustBeNumeric}
         distance_center2brake {mustBeNumeric}
 
@@ -16,46 +16,46 @@ classdef Axle
 
         force_matrix
         act_point_matrix
+
+        cross_section_radiuses
+        cross_section_change_area_positions
     end
     methods
-        function obj=Axle(length, distance_bearing, distance_center2brake, radius_main, radius_secondary, radius_brake, raduis_drive, radius_wheel, car)
-            import Solvers.*;
+        function obj=Axle(length_axle, distance_bearing, distance_center2brake, radius_main, radius_secondary, radius_brake, raduis_drive, radius_wheel, car)
+            import Car.*;
             if nargin == 9
-                obj.length;
-                obj.distance_bearing;
-                obj.distance_center2brake;
-                obj.radius_main;
-                obj.radius_secondary;
-                obj.radius_brake;
-                obj.raduis_drive;
-                obj.radius_wheel;
-                obj.car;
+                obj.length_axle = length_axle;
+                obj.distance_bearing = distance_bearing;
+                obj.distance_center2brake = distance_center2brake;
+                obj.radius_main = radius_main;
+                obj.radius_secondary = radius_secondary;
+                obj.radius_brake = radius_brake;
+                obj.raduis_drive = raduis_drive;
+                obj.radius_wheel = radius_wheel;
+                obj.car = car;
+
+                cross_section_radiuses = [obj.radius_secondary, obj.radius_main, obj.radius_secondary];
+                cross_section_change_area_positions = [0, obj.distance_bearing, obj.length_axle - obj.distance_bearing];
+                %Cross section information
+                % cross_section_moment_of_inertias = [
+                %     0, 0, 0;
+                %     axle_secondary_radius, axle_main_radius, axle_secondary_radius;
+                %     axle_secondary_radius, axle_main_radius, axle_secondary_radius
+                % ].^4 * pi / 4;
+
+                % cross_section_areas = [axle_secondary_radius, axle_main_radius, axle_secondary_radius].^2 * pi;
             end
         end
-        function update_load_constant_velocity(obj, new_velocity)
-            %Inputdata
-            L = 10;
-
-            %Veichle geometry
-            h = 1;  %CoM - Ground
-            h1 = 0.5; %CoM - Air resistance
-            df = 1; %CoM - front
-            db = 1; %CoM - rear
-            dh = 2; %
-            rd = 1; %radius chain drive
-            rb = 1; %radius brake rotor
-            Dd = 1; %radius wheel
-
+        function ret=update_load_constant_velocity(obj, new_velocity)
             %Constants
             air_desity = 1;
             g = 9.82;
 
-            car = obj.car;
             %Given forces
-            air_resistance_magnitude = 0.5 * air_desity * car.area_front * car.coefficient_air_resistance * new_velocity.^2;
-            drive_force_magnitude = air_resistance_magnitude + car.mass * 0;
-            wheel_force_vertical_magnitude = (drive_force_magnitude * car.height_center_of_mass + air_resistance_magnitude * car.height_air_resistance + car.mass * g * car.distance_front) / (car.distance_rear + car.distance_front);
-            chain_force_magnitude = drive_force_magnitude * (car.radius_wheel * 2) / (2 * obj.raduis_drive);
+            air_resistance_magnitude = 0.5 * air_desity * obj.car.area_front * obj.car.coefficient_air_resistance * new_velocity.^2;
+            drive_force_magnitude = air_resistance_magnitude + obj.car.mass * 0;
+            wheel_force_vertical_magnitude = (drive_force_magnitude * obj.car.height_center_of_mass + air_resistance_magnitude * obj.car.height_air_resistance + obj.car.mass * g * obj.car.distance_front) / (obj.car.distance_rear + obj.car.distance_front);
+            chain_force_magnitude = drive_force_magnitude * (obj.radius_wheel * 2) / (2 * obj.raduis_drive);
 
             %Directions
             right = [1; 0; 0];
@@ -72,17 +72,100 @@ classdef Axle
             %Act points
             wheel_act_point_left = 0 * right - obj.radius_wheel * up;
             bearing_act_point_left = (obj.distance_bearing) * right;
-            brake_act_point_left = (obj.length * 0.5 - obj.distance_center2brake) * right + (obj.radius_brake/sqrt(2)) * forward - (obj.radius_brake/sqrt(2)) * up;
-            chain_act_point = right * L/2 + obj.raduis_drive * up;
-            brake_act_point_right = (obj.length * 0.5 + obj.distance_center2brake) * right + (obj.radius_brake/sqrt(2)) * forward - (obj.radius_brake/sqrt(2)) * up;
-            bearing_act_point_right = (obj.length - obj.distance_bearing) * right;
-            wheel_act_point_right = obj.length * right - obj.radius_wheel * up;
+            brake_act_point_left = (obj.length_axle * 0.5 - obj.distance_center2brake) * right + (obj.radius_brake/sqrt(2)) * forward - (obj.radius_brake/sqrt(2)) * up;
+            chain_act_point = right * obj.length_axle/2 + obj.raduis_drive * up;
+            brake_act_point_right = (obj.length_axle * 0.5 + obj.distance_center2brake) * right + (obj.radius_brake/sqrt(2)) * forward - (obj.radius_brake/sqrt(2)) * up;
+            bearing_act_point_right = (obj.length_axle - obj.distance_bearing) * right;
+            wheel_act_point_right = obj.length_axle * right - obj.radius_wheel * up;
 
             wheel_total_force_N = wheel_normal_force_N + wheel_drive_force_N;
 
             %Forces at acting points on the axle
             obj.force_matrix = [wheel_total_force_N, bearing_force_N, braking_force_N, chain_force_N, braking_force_N, bearing_force_N, wheel_total_force_N];
             obj.act_point_matrix = [wheel_act_point_left, bearing_act_point_left, brake_act_point_left, chain_act_point, brake_act_point_right, bearing_act_point_right, wheel_act_point_right];
+
+            ret = obj;
+        end
+        function ret=update_load_acceleration(obj, new_velocity, new_acceleration)
+            %Constants
+            air_desity = 1;
+            g = 9.82;
+
+            %Given forces
+            air_resistance_magnitude = 0.5 * air_desity * obj.car.area_front * obj.car.coefficient_air_resistance * new_velocity.^2;
+            drive_force_magnitude = air_resistance_magnitude + obj.car.mass * new_acceleration;
+            wheel_force_vertical_magnitude = (drive_force_magnitude * obj.car.height_center_of_mass + air_resistance_magnitude * obj.car.height_air_resistance + obj.car.mass * g * obj.car.distance_front) / (obj.car.distance_rear + obj.car.distance_front);
+            chain_force_magnitude = drive_force_magnitude * (obj.radius_wheel * 2) / (2 * obj.raduis_drive);
+
+            %Directions
+            right = [1; 0; 0];
+            forward = [0; 1; 0];
+            up = [0; 0; 1];
+
+            %Forces
+            bearing_force_N = -0.5 * (drive_force_magnitude + chain_force_magnitude) * forward + wheel_force_vertical_magnitude * -0.5 * up ; % Actual values later
+            wheel_drive_force_N = drive_force_magnitude * 0.5 * forward;
+            wheel_normal_force_N = wheel_force_vertical_magnitude * 0.5 * up;
+            braking_force_N = 0 * (up + forward) / norm(up + forward);
+            chain_force_N = chain_force_magnitude * forward;
+
+            %Act points
+            wheel_act_point_left = 0 * right - obj.radius_wheel * up;
+            bearing_act_point_left = (obj.distance_bearing) * right;
+            brake_act_point_left = (obj.length_axle * 0.5 - obj.distance_center2brake) * right + (obj.radius_brake/sqrt(2)) * forward - (obj.radius_brake/sqrt(2)) * up;
+            chain_act_point = right * obj.length_axle/2 + obj.raduis_drive * up;
+            brake_act_point_right = (obj.length_axle * 0.5 + obj.distance_center2brake) * right + (obj.radius_brake/sqrt(2)) * forward - (obj.radius_brake/sqrt(2)) * up;
+            bearing_act_point_right = (obj.length_axle - obj.distance_bearing) * right;
+            wheel_act_point_right = obj.length_axle * right - obj.radius_wheel * up;
+
+            wheel_total_force_N = wheel_normal_force_N + wheel_drive_force_N;
+
+            %Forces at acting points on the axle
+            obj.force_matrix = [wheel_total_force_N, bearing_force_N, braking_force_N, chain_force_N, braking_force_N, bearing_force_N, wheel_total_force_N];
+            obj.act_point_matrix = [wheel_act_point_left, bearing_act_point_left, brake_act_point_left, chain_act_point, brake_act_point_right, bearing_act_point_right, wheel_act_point_right];
+
+            ret = obj;
+        end
+        function ret=update_load_braking(obj, new_velocity, new_acceleration)
+            %Constants
+            air_desity = 1;
+            g = 9.82;
+
+            %Given forces
+            air_resistance_magnitude = 0.5 * air_desity * obj.car.area_front * obj.car.coefficient_air_resistance * new_velocity.^2;
+            drive_force_magnitude = air_resistance_magnitude + obj.car.mass * new_acceleration;
+            wheel_force_vertical_magnitude = (drive_force_magnitude * obj.car.height_center_of_mass + air_resistance_magnitude * obj.car.height_air_resistance + obj.car.mass * g * obj.car.distance_front) / (obj.car.distance_rear + obj.car.distance_front);
+            chain_force_magnitude = drive_force_magnitude * (obj.radius_wheel * 2) / (2 * obj.raduis_drive);
+            chain_force_magnitude = drive_force_magnitude * (obj.radius_wheel * 2) / (2 * obj.radius_brake);
+
+            %Directions
+            right = [1; 0; 0];
+            forward = [0; 1; 0];
+            up = [0; 0; 1];
+
+            %Forces
+            bearing_force_N = -0.5 * (drive_force_magnitude + chain_force_magnitude) * forward + wheel_force_vertical_magnitude * -0.5 * up ; % Actual values later
+            wheel_drive_force_N = drive_force_magnitude * 0.5 * forward;
+            wheel_normal_force_N = wheel_force_vertical_magnitude * 0.5 * up;
+            braking_force_N = drive_force_magnitude * (up + forward) / norm(up + forward);
+            chain_force_N = chain_force_magnitude * forward;
+
+            %Act points
+            wheel_act_point_left = 0 * right - obj.radius_wheel * up;
+            bearing_act_point_left = (obj.distance_bearing) * right;
+            brake_act_point_left = (obj.length_axle * 0.5 - obj.distance_center2brake) * right + (obj.radius_brake/sqrt(2)) * forward - (obj.radius_brake/sqrt(2)) * up;
+            chain_act_point = right * obj.length_axle/2 + obj.raduis_drive * up;
+            brake_act_point_right = (obj.length_axle * 0.5 + obj.distance_center2brake) * right + (obj.radius_brake/sqrt(2)) * forward - (obj.radius_brake/sqrt(2)) * up;
+            bearing_act_point_right = (obj.length_axle - obj.distance_bearing) * right;
+            wheel_act_point_right = obj.length_axle * right - obj.radius_wheel * up;
+
+            wheel_total_force_N = wheel_normal_force_N + wheel_drive_force_N;
+
+            %Forces at acting points on the axle
+            obj.force_matrix = [wheel_total_force_N, bearing_force_N, braking_force_N, chain_force_N, braking_force_N, bearing_force_N, wheel_total_force_N];
+            obj.act_point_matrix = [wheel_act_point_left, bearing_act_point_left, brake_act_point_left, chain_act_point, brake_act_point_right, bearing_act_point_right, wheel_act_point_right];
+
+            ret = obj;
         end
 
 
@@ -118,6 +201,55 @@ classdef Axle
             ret.T = T;
             ret.M = M;
         end
+
+        function ret = calc_cross_section_stress(obj, x_points, cross_section_forces, cross_section_moments)
+            stress_matrixes = zeros(3, 3, length(x_points));
+            cross_section_moment_of_inertias = [
+                0, 0, 0;
+                cross_section_radiuses;
+                cross_section_radiuses
+            ].^4 * pi ./ 4;
+            cross_section_areas = pi * cross_section_radiuses.^2;
+            
+            for i = 1:length(x_points)
+                %Normal stress
+                z_coordinate = 0.1;
+                y_coordinate = 0.1;
+                
+                cs_force = cross_section_forces(:, i);
+                cs_moment = cross_section_moments(:, i);
+                cs_radius = cross_section_radiuses(1);
+                cs_area = cross_section_areas(1);
+                cs_inertia = cross_section_moment_of_inertias(:, 1);
+        
+                stress_xx = cs_force(1) / cs_area + y_coordinate * cs_moment(2) / cs_inertia(2) + z_coordinate * cs_moment(3) / cs_inertia(3);
+        
+                %Shear stress
+                % stress_xz = 4/3 * cs_force / cs_area;
+                % stress_xy = 4/3 * cs_force / cs_area;
+                %Stress xz = force-z direction - force y-direction
+                stress_xz = 4*cs_force(3) / (3*pi * cs_radius.^4) * (cs_radius.^2 - z_coordinate.^2) + 4*cs_force(2) / (3*pi * cs_radius.^4) * z_coordinate*y_coordinate;
+                stress_xy = 4*cs_force(2) / (3*pi * cs_radius.^4) * (cs_radius.^2 - y_coordinate.^2) - 4*cs_force(3) / (3*pi * cs_radius.^4) * y_coordinate*z_coordinate;
+                % stress_xy = -4 * cs_force(q) / (3 * pi * cs_radius.^4) * y_coordinate * z_coordinate;
+        
+                % stress_twist = G * Mv / Kr * r
+                stress_twist_magnitude = sqrt(y_coordinate.^2 * z_coordinate.^2) * cs_moment(1) / (cs_radius.^4 * pi / 2);
+                stress_twist = [0; -z_coordinate; y_coordinate];
+                stress_twist = stress_twist_magnitude * stress_twist / norm(stress_twist);
+                disp("Stress twist:   ");
+                disp(stress_twist);
+        
+                stress_matrix = [
+                    stress_xx, stress_xy, stress_xz;
+                    stress_xy, 0, 0;
+                    stress_xz, 0, 0
+                ];
+                stress_matrixes(:,:,i) = stress_matrix;
+                end
+        
+            ret = stress_matrixes;
+        end
+        
 
     end
 end
--- main.m
+++ main.m
@@ -1,5 +1,8 @@
 clear all; clc; close all;
 
+import Axle.*;
+import Car.*;
+
 %Inputdata
 L = 10;
 
@@ -11,63 +14,47 @@ db = 1; %CoM - rear
 dh = 2; %Wheel diameter
 rd = 1; %radius chain drive
 rb = 1; %radius brake rotor
-Dd = 1; %radius wheel
+
+b1 = 1; %Distance bearing
+bb = 1; %Distance brake disk from center
+
+D = 1; %Diameter main
+d = 0.6 * D; %Diameter secondary
+
+
+
+%Veichle data
+air_resistance_coefficient = 1;
+veichle_front_area = L*(h+h1);
 
 %Constants
 air_desity = 1;
-veichle_front_area = L*(h+h1);
-air_resistance_coefficient = 1;
-a = 0; %acceleration
-v = 10; %velocity
 mass = 100;
 g = 9.82;
 
-%Given forces
-air_resistance_magnitude = 0.5 * air_desity * veichle_front_area * air_resistance_coefficient * v * v;
-drive_force_magnitude = air_resistance_magnitude + mass * a;
-wheel_force_vertical_magnitude = (drive_force_magnitude * h + air_resistance_magnitude * h1 + mass * g * df) / (db + df);
-chain_force_magnitude = drive_force_magnitude * dh / (2 * rd);
 
-%Radiuses
-axle_main_radius = rb / 2;
-axle_secondary_radius = axle_main_radius * 0.8;
-chain_radius = rd; %rd
-wheel_radius = Dd; %Dd
-brake_disk_radius = rb; % rb
+%Inputs
+velocity = 10;
 
+car = Car(df, db, h1, h, L, mass, air_resistance_coefficient, veichle_front_area);
+axle = Axle(L, b1, bb, D/2, d/2, rb, rd, dh/2, car);
 
-
-
-%Cross section information
-cross_section_radiuses = [axle_secondary_radius, axle_main_radius, axle_secondary_radius];
-cross_section_change_area_position = [0, bearing_pos, L-bearing_pos];
-% cross_section_moment_of_inertias = [
-%     0, 0, 0;
-%     axle_secondary_radius, axle_main_radius, axle_secondary_radius;
-%     axle_secondary_radius, axle_main_radius, axle_secondary_radius
-% ].^4 * pi / 4;
-
-% cross_section_areas = [axle_secondary_radius, axle_main_radius, axle_secondary_radius].^2 * pi;
-
-
-
-
-
-
-
+axle = axle.update_load_constant_velocity(velocity);
 
 
 
 %Main program
 
-disp(force_matrix);
-disp(act_point_matrix);
+disp(axle)
+
+disp(axle.force_matrix);
+disp(axle.act_point_matrix);
 
 %Plot 3D visualization
-plot3(act_point_matrix(1, :), act_point_matrix(2, :), act_point_matrix(3, :), '*');
-for i = 1:length(force_matrix)
-    act_point = act_point_matrix(:, i);
-    force = force_matrix(:, i);
+plot3(axle.act_point_matrix(1, :), axle.act_point_matrix(2, :), axle.act_point_matrix(3, :), '*');
+for i = 1:length(axle.force_matrix)
+    act_point = axle.act_point_matrix(:, i);
+    force = axle.force_matrix(:, i);
     
     %Normalize force
     force = force / norm(force);
@@ -80,115 +67,86 @@ end
 %Draw cylinders
 %Main cylinder
 hold on;
-[X,Y,Z] = cylinder(axle_main_radius);
-surf(Z*(L - 2 * bearing_pos) + bearing_pos, X, Y);
+[X,Y,Z] = cylinder(axle.radius_main);
+surf(Z*(L - 2 * axle.distance_bearing) + axle.distance_bearing, X, Y);
 
 %Secondary cylinders
 hold on;
-[X,Y,Z] = cylinder(axle_secondary_radius);
-surf(Z*bearing_pos, X, Y);
+[X,Y,Z] = cylinder(axle.radius_secondary);
+surf(Z*axle.distance_bearing, X, Y);
 hold on;
-surf(Z*bearing_pos + L -bearing_pos, X, Y);
+surf(Z*axle.distance_bearing + L - axle.distance_bearing, X, Y);
 
 
 
 pbaspect([1,1,1]);
 
 
-
-function ret = calc_cross_section_stress(x_points, cross_section_forces, cross_section_moments, cross_section_radiuses, cross_section_change_area_position)
-    stress_matrixes = zeros(3, 3, length(x_points));
-    cross_section_moment_of_inertias = [
-        0, 0, 0;
-        cross_section_radiuses;
-        cross_section_radiuses
-    ].^4 * pi ./ 4;
-    cross_section_areas = pi * cross_section_radiuses.^2;
-    
-    for i = 1:length(x_points)
-        %Normal stress
-        z_coordinate = 0.1;
-        y_coordinate = 0.1;
-        
-        cs_force = cross_section_forces(:, i);
-        cs_moment = cross_section_moments(:, i);
-        cs_radius = cross_section_radiuses(1);
-        cs_area = cross_section_areas(1);
-        cs_inertia = cross_section_moment_of_inertias(:, 1);
-
-        stress_xx = cs_force(1) / cs_area + y_coordinate * cs_moment(2) / cs_inertia(2) + z_coordinate * cs_moment(3) / cs_inertia(3);
-
-        %Shear stress
-        % stress_xz = 4/3 * cs_force / cs_area;
-        % stress_xy = 4/3 * cs_force / cs_area;
-        %Stress xz = force-z direction - force y-direction
-        stress_xz = 4*cs_force(3) / (3*pi * cs_radius.^4) * (cs_radius.^2 - z_coordinate.^2) + 4*cs_force(2) / (3*pi * cs_radius.^4) * z_coordinate*y_coordinate;
-        stress_xy = 4*cs_force(2) / (3*pi * cs_radius.^4) * (cs_radius.^2 - y_coordinate.^2) - 4*cs_force(3) / (3*pi * cs_radius.^4) * y_coordinate*z_coordinate;
-        % stress_xy = -4 * cs_force(q) / (3 * pi * cs_radius.^4) * y_coordinate * z_coordinate;
-
-        % stress_twist = G * Mv / Kr * r
-        stress_twist_magnitude = sqrt(y_coordinate.^2 * z_coordinate.^2) * cs_moment(1) / (cs_radius.^4 * pi / 2);
-        stress_twist = [0; -z_coordinate; y_coordinate];
-        stress_twist = stress_twist_magnitude * stress_twist / norm(stress_twist);
-        disp("Stress twist:   ");
-        disp(stress_twist);
-
-        stress_matrix = [
-            stress_xx, stress_xy, stress_xz;
-            stress_xy, 0, 0;
-            stress_xz, 0, 0
-        ];
-        stress_matrixes(:,:,i) = stress_matrix;
-        end
-
-    ret = stress_matrixes;
-end
-
 % force_matrix = [
-%     0, 0, 0;
-%     0, 0, 0;
-%     1, -2, 1;
-% ];
-% act_point_matrix = [
-%     0, 0.5, 1;
-%     0, 0, 0;
-%     0, 0, 0;
-% ];
+    %     0, 0, 0;
+    %     0, 0, 0;
+    %     1, -2, 1;
+    % ];
+    % act_point_matrix = [
+        %     0, 0.5, 1;
+        %     0, 0, 0;
+        %     0, 0, 0;
+        % ];
+        
+        function plot_cross_section_forces_and_moments(axle)
+            xx = linspace(0, axle.length_axle, 100);
+            
+            result = axle.calc_cross_section_forces(xx);
+            disp(result.T)
+            disp(result.M)
 
-xx = linspace(0, L, 100);
+            subplot(2, 2, 1);
+            plot(xx, result.T(1,:), 'o-');
+            hold on;
+            plot(xx, result.M(1,:), 'o-');
+            hold on;
+            title("X direction (right)");
+            legend(["Tx", "Mx"])
+            
+            subplot(2, 2, 2);
+            plot(xx, result.T(2,:), 'o-');
+            hold on;
+            plot(xx, result.M(2,:), 'o-');
+            hold on;
+            title("Y direction (forward)");
+            legend(["Ty", "My"])
+            
+            subplot(2, 2, 3);
+            plot(xx, result.T(3,:), 'o-');
+            hold on;
+            plot(xx, result.M(3,:), 'o-');
+            hold on;
+            title("Z direction (up)");
+            legend(["Tz", "Mz"])
+        end
+        
 
-result = calc_cross_section_forces(xx, force_matrix, act_point_matrix);
-disp(result.T)
-disp(result.M)
-f2 = figure;
-subplot(2, 2, 1);
-plot(xx, result.T(1,:), 'o-');
-hold on;
-plot(xx, result.M(1,:), 'o-');
-hold on;
-title("X direction (right)");
-legend(["Tx", "Mx"])
+        velocity = 10;
+        acceleration = 1;
+        deacceleration = -1;
 
-subplot(2, 2, 2);
-plot(xx, result.T(2,:), 'o-');
-hold on;
-plot(xx, result.M(2,:), 'o-');
-hold on;
-title("Y direction (forward)");
-legend(["Ty", "My"])
+        f1 = figure("Name", "Constant velocity");
+        axle = axle.update_load_constant_velocity(velocity);
+        plot_cross_section_forces_and_moments(axle);
+        
+        f2 = figure("Name", "Acceleration");
+        axle = axle.update_load_acceleration(velocity, acceleration);
+        plot_cross_section_forces_and_moments(axle);
 
-subplot(2, 2, 3);
-plot(xx, result.T(3,:), 'o-');
-hold on;
-plot(xx, result.M(3,:), 'o-');
-hold on;
-title("Z direction (up)");
-legend(["Tz", "Mz"])
+        f3 = figure("Name", "Braking");
+        axle = axle.update_load_acceleration(velocity, deacceleration);
+        plot_cross_section_forces_and_moments(axle);
 
-% result = calc_cross_section_forces(0, force_matrix, act_point_matrix);
-% disp(result.T)
-% disp(result.M)
 
-result = calc_cross_section_stress(0.3, result.T, result.M, cross_section_radiuses, cross_section_change_area_position);
-disp("Stress matrixes: ")
-disp(result);
+        % result = calc_cross_section_forces(0, force_matrix, act_point_matrix);
+        % disp(result.T)
+        % disp(result.M)
+        
+        % result = calc_cross_section_stress(0.3, result.T, result.M, cross_section_radiuses, cross_section_change_area_position);
+        % disp("Stress matrixes: ")
+        % disp(result);
